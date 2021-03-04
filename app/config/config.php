bb31663d8ca78e7004915a92ac12ffdef226c995
tanasamarius <tanasamariusiasi@gmail.com>
2021-03-04 20:56:52 +0200

    first commit

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+//DB params, creditials to connect to database
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', '');
+
+//app root
+define('APPROOT', dirname(dirname(__FILE__)));
+//url root
+define('URLROOT', 'your url root');
+//site name
+define('SITENAME', 'your site name');
